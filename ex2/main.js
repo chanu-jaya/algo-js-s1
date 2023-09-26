9e38f992c3fa8a13d84bf4f9ea62c70014f885b5
chanu-jaya <chanu.jayawardena@gmail.com>
2023-09-26 17:36:34 +0200

    adding learning and exercice

---
+++ ex2/main.js
@@ -0,0 +1,15 @@
+var a = 4 
+var b = 4
+var c = 3
+
+switch(a) {
+    case 1:
+        console.log("b = 4")
+        break
+    case c :
+        console.log("c = 3")
+        break
+    default :
+        console.log("égal à rien")
+
+}
---
+++ learning/main.js
@@ -0,0 +1,5 @@
+alert("Bonjour, es-tu sûr de vouloir quitter le site ?")
+
+console.log("Bienvenue dans la console")
+
+var cardName = "Choixpeau"
